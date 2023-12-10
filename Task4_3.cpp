f2badeda96b737d8edd17ea8497e3a1b760be0e8
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-12-10 17:58:46 +0200

    Update Task4_3.cpp

--- Task4_3.cpp
+++ Task4_3.cpp
@@ -1,59 +1,43 @@
-
 #include <iostream>
 
 char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
 using namespace std;
 
-int gcd (int x , int y ){
-    if (( x%y)==0)
-    return y;
-    
-    return gcd ( y, x%y);
+int gcd(int x, int y) {
+    if ((x % y) == 0)
+        return y;
+
+    return gcd(y, x % y);
 }
 
-int modInverse(int A, int M) 
-{ 
+int modInverse(int A, int M) {
     for (int X = 1; X < M; X++) {
-        if ((A * X) % M == 1) 
+        if ((A * X) % M == 1)
             return X;
     }
 
     // If the loop completes without finding the inverse
     return -1;
-} 
+}
 
-int main()
-{
-    /*cout<<gcd(7,26);*/
-    
-    
-    int a ,b ;
-    a= 7 , b= 2;
-    
-    
+int main() {
+    int a, b;
+    a = 7, b = 2;
+
     string msg;
-    cin>> msg;
-    
+    cin >> msg;
+
     string deciphered;
-    
-    
-    for (int i ; i<msg.length() ; i++)
-    {
-        for (int j = 0 ; j < 26 ; j++){
-        if (msg[i]==alphabet[j])
-        
-            deciphered = deciphered + alphabet[ j % 26 +   modInverse(a, 26)  ] ;
+
+    for (int i = 0; i < msg.length(); i++) {
+        for (int j = 0; j < 26; j++) {
+            if (msg[i] == alphabet[j])
+                deciphered = deciphered + alphabet[(j - b + 26) % 26];
         }
     }
-    
-    
-    cout <<deciphered;
-    
-    
-    
+
+    cout << deciphered;
 
     return 0;
 }
-
