7eca48bad6f50e53704c5e36d40050d000b1710f
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-12-12 15:26:15 +0200

    Update Task4_3.cpp

--- Task4_3.cpp
+++ Task4_3.cpp
@@ -1,39 +1,43 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-using namespace std;
-
 int gcd(int x, int y) {
-    if ((x % y) == 0)
-        return y;
-
+    if (y == 0)
+        return x;
     return gcd(y, x % y);
 }
 
-int modInverse(int A, int M) {
-    for (int X = 1; X < M; X++) {
-        if ((A * X) % M == 1)
-            return X;
+int modInverse(int a, int m) {
+    for (int x = 1; x < m; x++) {
+        if ((a * x) % m == 1)
+            return x;
     }
-
-    // If the loop completes without finding the inverse
-    return -1;
+    return -1; // Modular inverse doesn't exist
 }
 
 int main() {
-    int a, b;
-    a = 7, b = 7;
+    int a = 7, b = 7;
 
     string msg;
-    cin >> msg;
+    getline(cin,msg);
 
     string deciphered;
 
+    int aInverse = modInverse(a, 26);
+
     for (int i = 0; i < msg.length(); i++) {
-        for (int j = 0; j < 26; j++) {
-            if (msg[i] == alphabet[j])
-                deciphered = deciphered + alphabet[(j - b + 26) % 26];
+        if (isalpha(msg[i])) {
+            char ch = toupper(msg[i]);
+            int index = 0;
+            while (ch != alphabet[index]) {
+                index++;
+            }
+            deciphered += alphabet[(aInverse * ((index - b +26)) % 26)];
+        } else {
+            deciphered += msg[i]; // Keep non-alphabetic characters unchanged
         }
     }
 
